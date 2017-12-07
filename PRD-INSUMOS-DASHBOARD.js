f3e90066800847c4ebfca574ef432c77b7f6a790
Practicante CI APT <practicanteciapt@prolamsa.com>
2017-12-07 16:01:44 -0600

    Se agregaron nuevas graficas multidimensionales por proveedor / se ordenaron las graficas año - mes

--- PRD-INSUMOS-DASHBOARD.js
+++ PRD-INSUMOS-DASHBOARD.js
@@ -30,11 +30,19 @@ require( ["js/qlik"], function ( qlik ) {
 	var app = qlik.openApp('PRD-INSUMOS.qvf', config);
 
 	//get objects -- inserted here --
+	app.getObject('QV22','EcvmDXt');
+	app.getObject('QV23','TFGDdG');
+	app.getObject('QV20','jTjph');
+	app.getObject('QV21','ncEwMb');
+	app.getObject('QV17','WLQwHm');
+	app.getObject('QV18','TyDFtZj');
+	app.getObject('QV15','cJjHtZ');
+	app.getObject('QV16','FGNCaKE');
 	app.getObject('QV19','gjKXgyP');
-	app.getObject('QV18','WLQwHm');
-	app.getObject('QV17','TyDFtZj');
-	app.getObject('QV16','cJjHtZ');
-	app.getObject('QV15','FGNCaKE');
+	
+	
+	
+	
 	app.getObject('QV14','BMhULD');
 	app.getObject('QV12','jhjEds');
 	app.getObject('QV08','FAvejXs');
